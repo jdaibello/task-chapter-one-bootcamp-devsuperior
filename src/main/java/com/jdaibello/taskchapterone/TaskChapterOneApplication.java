be9538e9ea810f6049ba7a6e369c46602ca06599
João Pedro <jpedrodaibello1999@outlook.com>
2020-10-02 08:48:53 -0300

    Project created

---
+++ src/main/java/com/jdaibello/taskchapterone/TaskChapterOneApplication.java
@@ -0,0 +1,13 @@
+package com.jdaibello.taskchapterone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskChapterOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskChapterOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdaibello/taskchapterone/TaskChapterOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdaibello.taskchapterone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskChapterOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
